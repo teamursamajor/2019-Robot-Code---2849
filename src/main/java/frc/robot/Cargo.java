98d03e322efb314d41bd59cc7ce32394ac23c830
Hershal Rami <hershalrami@gmail.com>
2019-01-17 12:26:56 -0500

    This commit will add skeleton Thread code for Cargo and Hatch

--- src/main/java/frc/robot/Cargo.java
+++ src/main/java/frc/robot/Cargo.java
@@ -1,6 +1,28 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Spark;
 
-public class Cargo implements UrsaRobot{
-    //TODO skeleton code
+public class Cargo implements Runnable, UrsaRobot{
+    private boolean running = false;
+    private Spark armMotor;
+
+    public Cargo(Spark motor) {
+        AutoTest.debugMessage("I'm running");
+        armMotor = motor;
+        startCargo();
+    }
+
+    private void startCargo() {
+        //TODO placeholder, synchronize on an object
+        synchronized(Boolean.TRUE){
+            if (running)
+				return;
+			running = true;
+        }
+        new Thread(this, "cargoThread").start();
+    }
+
+    public void run() {
+        // TODO place runnable code
+    }
 }
--- src/main/java/frc/robot/Hatch.java
+++ src/main/java/frc/robot/Hatch.java
@@ -1,5 +1,28 @@
 package frc.robot;
 
-public class Hatch {
-    //TODO skeleton code
+import edu.wpi.first.wpilibj.Spark;
+
+public class Hatch implements Runnable, UrsaRobot{
+    private boolean running = false;
+    private Spark hatchMotor;
+
+    public Hatch(Spark motor) {
+        AutoTest.debugMessage("I'm running");
+        hatchMotor = motor;
+        startHatch();
+    }
+
+    private void startHatch() {
+        //TODO placeholder, synchronize on an object
+        synchronized(Boolean.TRUE){
+            if (running)
+				return;
+			running = true;
+        }
+        new Thread(this, "hatchThread").start();
+    }
+
+    public void run() {
+        // TODO place runnable code
+    }
 }
