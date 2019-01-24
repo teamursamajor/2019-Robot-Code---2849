529addf0670de5b98f9923fbf5885f2a79d5ced6
teamursamajor <teamursamajor@gmail.com>
2019-01-24 12:50:41 -0500

    'We added an Arm class to the robot to make things easier to read'

---
+++ src/main/java/frc/robot/Arm.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * Add your docs here.
+ */
+public class Arm {
+
+    /* Positions for the arm:
+     * -Getting a hatch
+     * -Deploying a hatch (Rocket/Cargo Bay)
+     * -Default position
+     * -Carrying position?
+     * 
+     * These values are measured as angles
+     * Cant be set in code
+     * Do have getters for thes positions
+     */
+
+    //TODO - set angles
+
+    double getHatchPos = null;
+    double deployHatchPos = null;
+    double defaultPos = null;
+    double carryingPos = null;
+
+
+    public Arm (){
+
+    }
+
+
+    
+
+
+}
