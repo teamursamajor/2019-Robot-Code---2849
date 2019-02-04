7c6da8143dd624cbd07c3c6f32075968a87bded9
teamursamajor <teamursamajor@gmail.com>
2019-02-04 14:43:46 -0500

    This commit will add some temporary code for ClimbTask based on the code for SusanTask.

--- src/main/java/frc/tasks/ClimbTask.java
+++ src/main/java/frc/tasks/ClimbTask.java
@@ -3,7 +3,58 @@ package frc.tasks;
 public class ClimbTask extends Task {
 
     public enum ClimbMode {
-        
+        Grounded, Top;
+
+        /**
+         * This method takes the current drive state and iterates the control loop then
+         * returns the next drive order for Drive to use
+         * 
+         * @return DriveOrder containing the left and right powers
+         */
+
+        public ClimbOrder callLoop() {
+            // "this" refers to the enum that the method is in
+            switch (this) {
+            case Grounded:
+                return autoGoToAngle(0);
+            case Top:
+                return autoGoToAngle(-90.0);
+            }
+            return new ClimbOrder(0.0);
+        }
+
+        private ClimbOrder autoGoToAngle(double angle) {
+            System.out.println("Test print");
+            return new ClimbOrder(0.0);
+        }
+    }
+
+    /**
+     * This holds information about the current state of the robot. It holds values
+     * for power, velocity, and position for both the left and right side.
+     */
+    public static class ClimbState {
+        public static double power = 0.0, angle = 0.0;
+        public static long stateTime = System.currentTimeMillis();
+
+        public static void updateState(double power, double angle) {
+            ClimbState.power = power;
+            ClimbState.angle = angle;
+            stateTime = System.currentTimeMillis();
+        }
+
+    }
+
+    /**
+     * This is returned by the DriveTask and holds values for the new left and right
+     * powers to be set by Drive
+     */
+    public static class ClimbOrder {
+        public double power;
+
+        public ClimbOrder(double power) {
+            this.power = power;
+        }
     }
 
     public ClimbTask() {
@@ -13,5 +64,5 @@ public class ClimbTask extends Task {
     public void run() {
 
     }
-    //TODO fill this out
+    // TODO fill this out
 }
--- src/main/java/frc/tasks/SusanTask.java
+++ src/main/java/frc/tasks/SusanTask.java
@@ -46,7 +46,6 @@ public class SusanTask extends Task {
         } 
     }
 
-
     public SusanTask(){
         super();
     }
