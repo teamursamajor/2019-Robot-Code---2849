17ac4f3773cc3c7c250b38197718ccb0ce37c0b6
teamursamajor <teamursamajor@gmail.com>
2019-02-05 12:31:34 -0500

    This commit will add neccessary code to HatchTask, TurnTask, and SusanTask

--- src/main/java/frc/tasks/TurnTask.java
+++ src/main/java/frc/tasks/TurnTask.java
@@ -6,15 +6,34 @@ public class TurnTask extends Task {
         TURN_TO, TURN_BY
     }
 
+    public TurnOrder callLoop() {
+        //"this" refers to the enum that the method is in
+        switch (this) {
+        case TURN_TO:
+            return autoGoToAngle(90);
+        case TURN_BY:
+            return autoGoToAngle(/*value here*/)
+        }
+        return new TurnOrder(0.0);
+    }
+
+    private TurnOrder autoGoToAngle(double angle) {
+        return new TurnOrder(0.0);
+    }
+
     public TurnTask() {
         super();
     }
+
+    public static boolean running = true;
 
     public void run() {
         while(running) {
             try {
                 Thread.sleep(20);
-            } cat
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
