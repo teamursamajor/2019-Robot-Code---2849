b215630962e360645f881ff1c7023da61cff1eda
teamursamajor <teamursamajor@gmail.com>
2019-02-18 17:54:35 -0500

    changed climb time

--- src/main/java/frc/robot/Climb.java
+++ src/main/java/frc/robot/Climb.java
@@ -4,8 +4,16 @@ import edu.wpi.first.wpilibj.Spark;
 
 public class Climb implements UrsaRobot {
 
+
     private Spark climbFrontMotor;
     private Spark climbBackMotor;
+    private boolean ClimbStop;
+    private boolean ClimbIsRunning;
+    // Climb time is in milliseconds. 
+    private static final int FrontClimbTimeInit = 1000;
+    private static final int FrontClimbTimeEnd = 1000;
+    private static final int BackClimbTime = 1500;
+
 
     public Climb() {
         climbFrontMotor = new Spark(CLIMB_FRONT);
@@ -13,12 +21,88 @@ public class Climb implements UrsaRobot {
     }
 
     public void climbInit() {
-        Thread t = new Thread("Climber Thread");
+        ClimbStop = false;
+        ClimbIsRunning = true;
+        Thread t = new Thread(new Runnable(){
+
+            public void run() {
+                climb();
+            }
+            
+        });    
         t.start();
     }
 
-    public void run() {
-        
+    private void climb() 
+    {
+        //start the front motor
+        long start = System.currentTimeMillis();
+        climbFrontMotor.set(Constants.climbPower);
+        while(!ClimbStop && (System.currentTimeMillis() - start) < FrontClimbTimeInit)
+        {
+            try
+            {
+                Thread.sleep(10);
+            }
+            catch(Exception ex)
+            {
+            
+            }
+        }
+
+        if(ClimbStop)
+        {
+            stopMotors();
+            return;
+        }
+
+        //front is moving, now start the back
+        climbBackMotor.set(Constants.climbPower);
+        while(!ClimbStop && (System.currentTimeMillis() - start) < FrontClimbTimeEnd)
+        {
+            try
+            {
+                Thread.sleep(10);
+            }
+            catch(Exception ex)
+            {
+            
+            }
+        }
+
+        if(ClimbStop)
+        {
+            stopMotors();
+            return;
+        }
+
+        //stop the front
+        climbFrontMotor.set(0);
+        //but keep the back going
+        while(!ClimbStop && (System.currentTimeMillis() - start) < BackClimbTime)
+        {
+            try
+            {
+                Thread.sleep(10);
+            }
+            catch(Exception ex)
+            {
+            
+            }
+        }
+
+        stopMotors();
+        ClimbIsRunning = false;
+    }
+
+    public void cancelClimb()
+    {
+        ClimbStop = true;
+    }
+
+    public boolean isClimbing()
+    {
+        return ClimbIsRunning;
     }
 
     public void setFrontMotor(double power) {
