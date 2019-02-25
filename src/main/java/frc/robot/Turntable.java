707215ecf4f9f62f593f18119b572cf326afb45f
teamursamajor <teamursamajor@gmail.com>
2019-02-25 15:36:19 -0500

    Update

--- src/main/java/frc/robot/Turntable.java
+++ src/main/java/frc/robot/Turntable.java
@@ -2,41 +2,46 @@ package frc.robot;
 
 import frc.tasks.*;
 import frc.tasks.TurntableTask.TurntableMode;
+import edu.wpi.first.networktables.NetworkTableEntry;
 
 import edu.wpi.first.wpilibj.Spark;
 
 public class Turntable extends Subsystem<TurntableTask.TurntableMode> implements UrsaRobot {
 
     private Spark turntableMotor;
-    private boolean targetFound;
-    private int defaultPipeline = 1;
+    private boolean turntableCamera = false;
 
     public Turntable() {
         turntableMotor = new Spark(TURNTABLE);
         subsystemMode = TurntableMode.CUSTOM;
-        targetFound = false;
     }
 
     public void runSubsystem() {
         TurntableTask.TurntableOrder turntableOrder = subsystemMode.callLoop();
         turntableMotor.set(turntableOrder.power);
 
+    
+
         // TODO test code
-        // else if (xbox.getButton(XboxController.BUTTON_LEFTSTICK)){
-        //     //Auto mode
-        //     NetworkTableEntry entry = UrsaRobot.limelightTable.getEntry("pipeline");
-        
-        //     if (entry.getDouble(UrsaRobot.defaultCameraPipeline) != UrsaRobot.visionCameraPipeline);
-        //     //check auto pipe line in the lime light, enable if need be
-        // //see if there are targets in view, if not set motors to zero
-        //     //ALSO check to make sure we won't hit the eboard
-        //     //
-        //     //see if we need to move left or right
-        //     //
-        // }
-        // else {
-        //     turntableMotor.set(0.0);
-        // }
+        if (xbox.getButton(XboxController.BUTTON_LEFTSTICK)) {
+            turntableCamera = !turntableCamera;
+        }
+
+        if (turntableCamera) {
+            //Auto mode
+            NetworkTableEntry pipeEntry = UrsaRobot.limelightTable.getEntry("getpipe");
+            NetworkTableEntry targetEntry = UrsaRobot.limelightTable.getEntry("tv");
+
+            if (pipeEntry.getDouble(UrsaRobot.defaultCameraPipeline) != UrsaRobot.visionCameraPipeline) {
+                UrsaRobot.limelightTable.getEntry("getpipe").setDouble(UrsaRobot.visionCameraPipeline);
+            }
+            if (targetEntry.getDouble(0) != 1) {
+                turntableMotor.set(0.0);
+            }
+        }
+        // TODO make loop
+        // ALSO check to make sure we won't hit the eboard and see if we need to move left or right
+
     }
 
 }
