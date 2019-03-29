cb6a720520f8e662762d2284e7786adf764e88cb
teamursamajor <teamursamajor@gmail.com>
2019-03-29 16:18:04 -0400

    This commit will improve readability

--- src/main/java/frc/robot/ControlMap.java
+++ src/main/java/frc/robot/ControlMap.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 
 /**
  * This is a map of all of the controls for the robot.
+ *<ul>
+ *<li><b>PUT CONTROLS HERE</b></li>
+ *</ul>
  */
 public class ControlMap {
 
@@ -35,7 +38,7 @@ public class ControlMap {
         // Auto Align
         map.put("auto_align", XboxController.POV_UP);
         map.put("cancel_auto_align", XboxController.POV_DOWN);
-        map.put("limelight_toggle", XboxController.POV_RIGHT);
+        map.put("limelight toggle", XboxController.POV_RIGHT);
 
         // Climb
         map.put("climb_leadscrew_up", XboxController.BUTTON_START);
