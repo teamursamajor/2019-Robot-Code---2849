3d5aad188e79113294eb3b3e0e30d3a14c77f357
Hershal Rami <hershalrami@gmail.com>
2019-04-08 16:35:44 -0400

    this commit will update pathfollower

--- src/main/java/frc/robot/PathFollower.java
+++ src/main/java/frc/robot/PathFollower.java
@@ -17,79 +17,65 @@ import jaci.pathfinder.followers.EncoderFollower;
 public class PathFollower extends TimedRobot implements UrsaRobot {
     private static final int k_ticks_per_rev = 1024;
 
-    private static final double k_wheel_diameter = 4.0 / 12.0;
+    private static final double k_wheel_diameter = 0.2032;
     private static final double k_max_velocity = 160;
 
-    // private static final int k_gyro_port = 0;
     private SpeedController m_left_motor, m_right_motor;
 
     private Encoder m_left_encoder, m_right_encoder;
 
-    private Drive drive; // TODO - replace with navx
+    private Drive drive;
 
-    private EncoderFollower leftFollower, rightFollower; // TODO - CHANGE
+    private EncoderFollower leftFollower, rightFollower;
 
     private Notifier followNotifier;
     private Trajectory lTrajectory, rTrajectory;
 
     /**
      * 
-     * @param leftEncoder encoder on the left side
+     * @param leftEncoder  encoder on the left side
      * @param rightEncoder encoder on the right side
-     * @param leftMotor motor on the left side
-     * @param rightMotor motor on the right side
+     * @param leftMotor    motor on the left side
+     * @param rightMotor   motor on the right side
      */
-    public PathFollower(Drive drive){
+    public PathFollower(Drive drive) {
         this.drive = drive;
         m_left_encoder = UrsaRobot.leftEncoder;
         m_right_encoder = UrsaRobot.rightEncoder;
-        m_left_motor = drive.mFrontLeft;
-        m_right_motor = drive.mFrontRight;
+        m_left_motor = Drive.mFrontLeft;
+        m_right_motor = Drive.mFrontRight;
     }
 
     /**
-     * Call this to follow a path
-     * @param path
+     * Call this to follow a path. It configures the encoder followers and the PID
+     * loop
+     * 
+     * @param path Name of the path
      * @throws IOException
      */
-    public void runPath(String path) throws IOException{
-        setFollowers(path);
-        followNotifier = new Notifier(this::followPath);
-        followNotifier.startPeriodic(lTrajectory.get(0).dt);
-    }
-
-    /**
-     * configures the encoder followers
-     */
-    private void setFollowers(String path) throws IOException{
-        lTrajectory = PathfinderFRC.getTrajectory(path + ".left");
-        rTrajectory = PathfinderFRC.getTrajectory(path + ".right");
+    public void pathInit(String path) throws IOException {
+        lTrajectory = PathfinderFRC.getTrajectory(path + ".right");
+        rTrajectory = PathfinderFRC.getTrajectory(path + ".left");
 
         leftFollower = new EncoderFollower(lTrajectory);
         rightFollower = new EncoderFollower(rTrajectory);
 
-        //TODO - find these values imperiacally
+        // TODO - find these values empirically
         double kp = 0.0;
         double ki = 0.0;
         double kd = 0.0;
-        double kv = 1/k_max_velocity;
+        double kv = 1 / k_max_velocity;
         double ka = 0.0;
-        setFollowers(kp,ki,kd,kv,ka);
 
-    }
-
-
-    private void setFollowers(double kp, double ki, double kd, double kv, double ka){
         leftFollower.configureEncoder(m_left_encoder.get(), k_ticks_per_rev, k_wheel_diameter);
-        leftFollower.configurePIDVA(kp,ki,kd,kv,ka);
+        leftFollower.configurePIDVA(kp, ki, kd, kv, ka);
 
         rightFollower.configureEncoder(m_right_encoder.get(), k_ticks_per_rev, k_wheel_diameter);
-        rightFollower.configurePIDVA(kp,ki,kd,kv,ka);
+        rightFollower.configurePIDVA(kp, ki, kd, kv, ka);
+
+        followNotifier = new Notifier(this::followPath);
+        followNotifier.startPeriodic(lTrajectory.get(0).dt);
     }
-    
-    // @Override
-    // public void autonomousInit() {
-    // }
 
     private void followPath() {
         if (leftFollower.isFinished() || rightFollower.isFinished()) {
@@ -98,29 +84,24 @@ public class PathFollower extends TimedRobot implements UrsaRobot {
             double left_speed = leftFollower.calculate(leftEncoder.get());
             double right_speed = rightFollower.calculate(rightEncoder.get());
             double heading = drive.getHeading();
+
+            // this may need to be negative, try it if the robot is turning the wrong way or something
             double desired_heading = Pathfinder.r2d(leftFollower.getHeading());
+            
             double heading_difference = Pathfinder.boundHalfDegrees(desired_heading - heading);
-            //TODO - check
+            
+            // these numbers are from WPILIB, dont know why/where they came from
             double turn = 0.8 * (-1.0 / 80.0) * heading_difference;
+            
             m_left_motor.set(left_speed + turn);
             m_right_motor.set(right_speed - turn);
         }
     }
 
-    /**
-     * This function is called periodically during autonomous.
-     */
-    @Override
-    public void autonomousPeriodic() {
-    }
-
-    /**
-     * stops path follower before it reaches its end goal
-     */
-    @Override
-    public void teleopInit() {
+    public void killPath() {
         followNotifier.stop();
         m_left_motor.set(0);
         m_right_motor.set(0);
+        Drive.stop();
     }
 }
